454fec051ce4a507a520aa8d0d972a84075df7ea
Ayasouk <ayasouk.iam@gmail.com>
2014-10-15 20:29:10 +0200

    Update main.c
    Ajouter une fonction Init_Automate qui initialise la matrice representant l'automate cellulaire (ttes les cellules commencent mortes)

--- TP3-ALGO-S3/main.c
+++ TP3-ALGO-S3/main.c
@@ -4,6 +4,22 @@
  *
  * Created on 15 octobre 2014, 17:03
  */
+/*definition of the struct cell (explain the state of the cell)*/
+typedef enum { VIVANTE='¤', MORTE='*' } Cell;
+typedef struct { int Line, Row ; } Coord; //position of a plan(or matrice)
+
+
+void Init_Automate(Cell Automate[N][N]){
+	int i, j;
+	for(i=0; i<N; i++){
+		for(j=0; j<N; j++){
+			Automate[i][j]=MORT;
+		}
+	}
+}
+
+void Indicate_Cells(Cell Automate[N][N], Coord *PositionCell){
+}
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,7 +28,11 @@
  * 
  */
 int main(int argc, char** argv) {
-
+	Cell Automate[N][N];
+	Coord PositionCell;
+	
+	Init_Automate(Automate);
+	Indicate_Cells(Automate, &PositionCell);
 	printf("Hello World!");
 	return EXIT_SUCCESS;
 }
